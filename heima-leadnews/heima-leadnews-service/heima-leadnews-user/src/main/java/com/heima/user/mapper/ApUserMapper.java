f26988acdb8328f2bdc8e00413d186664eb923ed
“yangjianingpaul” <yangjianingpaul@icloud.com>
2024-02-03 01:13:21 +0800

    mapper

--- heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserMapper.java
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserMapper.java
@@ -2,7 +2,6 @@ package com.heima.user.mapper;
 
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.heima.model.admin.pojos.ApUserRealname;
 import com.heima.model.user.pojos.ApUser;
 import org.apache.ibatis.annotations.Mapper;
 
--- heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserRealNameMapper.java
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserRealNameMapper.java
@@ -2,6 +2,8 @@ package com.heima.user.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.heima.model.admin.pojos.ApUserRealname;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ApUserRealNameMapper extends BaseMapper<ApUserRealname> {
 }
