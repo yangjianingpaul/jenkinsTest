4199e34777da4d729d01398bd32af7f659b71120
“yangjianingpaul” <yangjianingpaul@icloud.com>
2024-02-03 00:54:17 +0800

    application

--- heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -3,11 +3,12 @@ package com.heima.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @MapperScan("com.heima.user.mapper")
 @EnableFeignClients(basePackages = "com.heima.apis")
