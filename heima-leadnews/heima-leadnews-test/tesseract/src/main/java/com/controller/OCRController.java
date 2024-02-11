021769fa56bbc416784a2f3c3624986d410fc5ff
“yangjianingpaul” <yangjianingpaul@icloud.com>
2024-02-11 16:08:32 +0800

    tesseract-5

---
+++ heima-leadnews/heima-leadnews-test/tesseract/src/main/java/com/controller/OCRController.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+@RestController
+public class OCRController {
+
+    @GetMapping("ocr")
+    public ResponseEntity<String> performOCR(@RequestParam String url2) throws IOException, TesseractException {
+        Tesseract instance = new Tesseract();
+        instance.setDatapath("/usr/share/tesseract-ocr/5/tessdata");
+        instance.setLanguage("chi_sim+eng");
+        URL url = new URL(url2);
+        BufferedImage image = ImageIO.read(url);
+        String result = instance.doOCR(image);
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+}
