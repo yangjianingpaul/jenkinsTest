cb9f7fde89f0c063ecd65f71370faf2b3e4f2b41
“yangjianingpaul” <yangjianingpaul@icloud.com>
2024-02-11 13:27:25 +0800

    tesseract

--- heima-leadnews/heima-leadnews-test/tesseract/src/main/java/Application.java
+++ heima-leadnews/heima-leadnews-test/tesseract/src/main/java/Application.java
@@ -6,9 +6,9 @@ import java.io.File;
 public class Application {
     public static void main(String[] args) throws TesseractException {
         Tesseract instance = new Tesseract();
-        instance.setDatapath("/Users/paulyang/Desktop/tessdata");
+        instance.setDatapath("/usr/local/tessdata");
         instance.setLanguage("chi_sim");
-        File file = new File("/Users/paulyang/Desktop/test.png");
+        File file = new File("/usr/local/helloworld.png");
         String result = instance.doOCR(file);
         System.out.println(result);
     }
